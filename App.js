a4613100dade087bb390db4564142e5c1932641e
Umesh <umeshraj1000@gmail.com>
2021-10-02 18:12:51 +0530

    homescreen

--- App.js
+++ App.js
@@ -1,23 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 import Entypo from "react-native-vector-icons/Entypo";
+import Home from "./src/screens/Home";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Entypo name="home" />
-      <StatusBar style="auto" />
+    <View>
+      <StatusBar translucent={false} backgroundColor="white" />
+      <Home />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ src/screens/Home.js
@@ -0,0 +1,80 @@
+import React from "react";
+import {
+  View,
+  StyleSheet,
+  Text,
+  ImageBackground,
+  Dimensions,
+  Pressable,
+} from "react-native";
+import Fontisto from "react-native-vector-icons/Fontisto";
+
+function Home(props) {
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        source={require("../../assets/images/wallpaper.jpg")}
+        style={styles.image}
+      >
+        <Pressable style={styles.searchButton}>
+          <Fontisto name="search" size={25} color={"#f15454"} />
+          <Text style={styles.searchButtonText}>Where are you going?</Text>
+        </Pressable>
+        <Text style={styles.title}>Go Near</Text>
+        <Pressable style={styles.button}>
+          <Text style={styles.buttonText}>Expolore nearby stays</Text>
+        </Pressable>
+      </ImageBackground>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {},
+  image: {
+    width: "100%",
+    height: 500,
+    resizeMode: "cover",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 80,
+    fontWeight: "bold",
+    color: "white",
+    width: "70%",
+    marginLeft: 25,
+  },
+  button: {
+    backgroundColor: "white",
+    width: 200,
+    marginLeft: 25,
+    height: 40,
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 25,
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  searchButton: {
+    backgroundColor: "white",
+    width: "100%",
+    height: 60,
+    marginHorizontal: 10,
+    borderRadius: 30,
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+    position: "absolute",
+    top: 20,
+    width: Dimensions.get("window").width - 20,
+  },
+  searchButtonText: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+});
+
+export default Home;
